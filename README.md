58bf8499a78cb5ee31b3d6d609d99f115f9d4c1c
Luca Casonato <hello@lcas.dev>
2024-06-14 09:33:55 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## runtime-detector
+
+This package detects what runtime you are in.
+
+```ts
+import { detect } from "@luca/runtime-detector";
+
+console.log(detect()); // "deno"
+```
---
+++ index.ts
@@ -0,0 +1,10 @@
+/**
+ * This function detects which runtime you are executing in.
+ * @returns The runtime you are in, and null if the runtime could not be detected.
+ */
+export function detect(): "deno" | "node" | "browser" | null {
+  if ("Deno" in globalThis) return "deno";
+  if ("process" in globalThis) return "node";
+  if ("document" in globalThis) return "browser";
+  return null;
+}
